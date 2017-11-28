46f80a7c572a224ace9da7ed7422ee7c517465b5
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2017-11-28 13:17:27 -0500

    MergeSort implementation

--- SortingVisualization/src/application/Main.java
+++ SortingVisualization/src/application/Main.java
@@ -1,30 +1,49 @@
 package application;
 	
-import javafx.application.Application;
-import javafx.stage.Stage;
+import Lab3_2.ArrayMethods2;
+import javafx.application.Application; 
+import javafx.scene.Group; 
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage; 
+import javafx.scene.text.Text; 
+import javafx.scene.control.Button;
 
-/**
- * Sorting Visualization
- * @author Alex Feng, Raymond Cheung
- */
-
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-}
+public class Main extends Application { 
+	private Button btnMergeSort;
+   @Override 
+   public void start(Stage stage) {  
+	   //btnMergeSort.setLayoutX(50);
+	  //btnMergeSort.setLayoutY(50);
+	  //btnMergeSort.setText("hi");
+	   
+	   	String[] test1 = {"test", "zebra", "lol", "year"};
+	   	ArrayMethods2.mergeSort(test1);
+      //Creating a Text object 
+      Text text = new Text();      
+      
+      //Setting the text to be added. 
+    	  text.setText(test1[2]);
+       
+      //setting the position of the text 
+      text.setX(50); 
+      text.setY(50); 
+         
+      //Creating a Group object  
+      Group root = new Group(text);   
+               
+      //Creating a scene object 
+      Scene scene = new Scene(root, 600, 300);  
+     
+      
+      //Setting title to the Stage 
+      stage.setTitle("Sorting Visualizations"); 
+      //Adding scene to the stage 
+      stage.setScene(scene); 
+      //Displaying the contents of the stage 
+      stage.show(); 
+   }      
+   
+   public static void main(String args[]){ 
+      launch(args); 
+   } 
+} 
